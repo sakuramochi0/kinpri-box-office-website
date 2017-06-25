ab877e9b4cbe5886b702affa3a1e85dd8f8f7451
sakuramochi <sakuramochi.mochi.0@gmail.com>
2017-06-25 19:24:17 +0900

    add korean weekly chart and kinpri1 charts

--- views.py
+++ views.py
@@ -18,7 +18,8 @@ def index(request):
 def api(request, endpoint):
     db = get_mongo_client().kinpri_box_office
 
-    if endpoint == 'v1/mimorin/daily.json':
+    if endpoint == 'v1/mimorin/daily.json' or \
+       endpoint == 'v2/kinpri2/daily.json':
         # make header
         data = [[
             {'id': '_id', 'label': '日付', 'type': 'string'},
@@ -42,7 +43,8 @@ def api(request, endpoint):
         date = date + datetime.timedelta(days=1)
         date = date.strftime('%-m/%-d({})'.format(get_weekday(date)))
         data.append([date, 0, None])
-    elif endpoint == 'v1/mimorin/weekly.json':
+    elif endpoint == 'v1/mimorin/weekly.json' or \
+         endpoint == 'v2/kinpri2/weekly.json':
         # make header
         data = [[
             {'id': '_id', 'label': '上映週', 'type': 'string'},
@@ -52,7 +54,7 @@ def api(request, endpoint):
 
         # add rows
         today = datetime.datetime.today()
-        publish_date = parse('6/10')
+        publish_date = parse('2017/6/10')
         weeks = (today - publish_date).days // 7 + 1
         for week in range(weeks):
             start = publish_date + datetime.timedelta(days=week * 7)
@@ -81,7 +83,66 @@ def api(request, endpoint):
                 data.append(row)
         # additional empty data
         data.append(['第{}週'.format(len(data)), 0, None])
-    elif endpoint == 'v1/korea/daily.json':
+    elif endpoint == 'v2/kinpri/daily.json':
+        # make header
+        data = [[
+            {'id': '_id', 'label': '日付', 'type': 'string'},
+            {'id': 'sell', 'label': '販売座席数', 'type': 'number'},
+            {'id': 'show_num', 'label': '上映回数', 'type': 'number'},
+        ]]
+        
+        # add rows
+        for i in db.mimorin_daily_kinpri1.find().sort('_id'):
+            date = i['_id']
+            row = [
+                date.strftime('%-m/%-d({})'.format(get_weekday(i['_id']))),
+                i['sell'],
+                i['show_num'],
+            ]
+            data.append(row)
+        # additional empty data
+        date = date + datetime.timedelta(days=1)
+        date = date.strftime('%-m/%-d({})'.format(get_weekday(date)))
+        data.append([date, 0, None])
+    elif endpoint == 'v2/kinpri/weekly.json':
+        # make header
+        data = [[
+            {'id': '_id', 'label': '上映週', 'type': 'string'},
+            {'id': 'sell', 'label': '販売座席数', 'type': 'number'},
+            {'id': 'show_num', 'label': '上映回数', 'type': 'number'},
+        ]]
+
+        # add rows
+        last_date = parse('2017/3/17') # last date recorded
+        publish_date = parse('2016/1/9')
+        weeks = (last_date - publish_date).days // 7 + 1
+        for week in range(weeks):
+            start = publish_date + datetime.timedelta(days=week * 7)
+            end = start + datetime.timedelta(days=6)
+            if week == weeks - 1: # latest week
+                haxes_label = '第{}週\n{}〜'.format(week + 1,
+                                                      start.strftime('%Y/%-m/%-d'))
+            else:
+                haxes_label = '第{}週\n{}〜{}'.format(week + 1,
+                                                      start.strftime('%Y/%-m/%-d'),
+                                                      end.strftime('%Y/%-m/%-d'))
+            cursor = db.mimorin_daily_kinpri1.aggregate([{
+                '$match': {'_id': {'$gte': start, '$lte': end}},
+            }, {
+                '$group': {'_id': haxes_label,
+                           'sell': {'$sum': '$sell'},
+                           'show_num': {'$sum': '$show_num'}},
+            }])
+            if cursor.alive:
+                i = cursor.next()
+                row = [
+                    i['_id'],
+                    i['sell'],
+                    i['show_num'],
+                ]
+                data.append(row)
+    elif endpoint == 'v1/korea/daily.json' or \
+         endpoint == 'v2/korea-kinpri2/daily.json':
         # make header
         data = [[
             {'id': '_id', 'label': '日付', 'type': 'string'},
@@ -101,6 +162,45 @@ def api(request, endpoint):
         date = i['_id'] + datetime.timedelta(days=1)
         date = date.strftime('%-m/%-d({})'.format(get_weekday(date)))
         data.append([date, 0, None])
+    elif endpoint == 'v2/korea-kinpri2/weekly.json':
+        # make header
+        data = [[
+            {'id': '_id', 'label': '上映週', 'type': 'string'},
+            {'id': 'sell', 'label': '販売座席数', 'type': 'number'},
+            {'id': 'box_office', 'label': '興行収入', 'type': 'number'},
+        ]]
+
+        # add rows
+        today = datetime.datetime.today()
+        publish_date = parse('2017/6/10')
+        weeks = (today - publish_date).days // 7 + 1
+        for week in range(weeks):
+            start = publish_date + datetime.timedelta(days=week * 7)
+            end = start + datetime.timedelta(days=6)
+            if week == weeks - 1: # latest week
+                haxes_label = '第{}週\n{}〜'.format(week + 1,
+                                                      start.strftime('%-m/%-d'))
+            else:
+                haxes_label = '第{}週\n{}〜{}'.format(week + 1,
+                                                      start.strftime('%-m/%-d'),
+                                                      end.strftime('%-m/%-d'))
+            cursor = db.korea.aggregate([{
+                '$match': {'_id': {'$gte': start, '$lte': end}},
+            }, {
+                '$group': {'_id': haxes_label,
+                           'sell': {'$sum': '$sell'},
+                           'box_office': {'$sum': '$box_office'}},
+            }])
+            if cursor.alive:
+                i = cursor.next()
+                row = [
+                    i['_id'],
+                    i['sell'],
+                    i['box_office'],
+                ]
+                data.append(row)
+        # additional empty data
+        data.append(['第{}週'.format(len(data)), 0, None])
     else:
         data = []
     return JsonResponse(data, safe=False)
